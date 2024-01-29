fcaf57b79da39568b3bf684c38d95bad87d62933
Pedro Gabriel <bielpedro300@gmail.com>
2024-01-29 11:18:35 -0300

    First Commit

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":"https://t.ctcdn.com.br/Nj2y1rY3L0yh2Zgros-SMuWFN4k=/1024x576/smart/i325730.png",
+    "title":"MONSTER HUNTER WORLD",
+    "description":"Discover the new expansion coming to the Monster Hunter World game. The DLC Iceborne comes with new monsters to hunt."
+  },
+  {
+    "id":"2",
+    "photo":"https://www.cyberpunk.net/build/images/pre-order/buy-b/keyart-ue-pt-br@2x-cd66fd0d.jpg",
+    "title":"Cyberpunk 2077: Phantom Liberty the new DLC for Cyberpunk 2077",
+    "description":"Cyberpunk 2077 new expansion should be coming soon on all plataforms..."
+  },
+  {
+    "id":"3",
+    "photo":"https://t.ctcdn.com.br/fH1yxz9Fyx2KPvMdFfe19D4-Fgs=/1200x675/smart/i528712.jpeg",
+    "title":"When the new title for the Elder Scrolls series coming out...",
+    "description":"Elder Scrolls 6 was announced a few years ago..."
+  },
+  {
+    "id":"4",
+    "photo":"https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/software/switch/70010000033071/3f7ee6aa3482b514bd443e116022b038a9728f017916ed37da3f09f731a7d5f2",
+    "title":"The Witcher 3 is coming out on the console Switch",
+    "description":"The 2017 Game of The Year The Witcher 3 is coming out on Switch."
+  }
+]
